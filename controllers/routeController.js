2ec79ffef3720f574107d7659a54f766297b6f37
Abdul Raheem <actekraheem@gmail.com>
2018-06-26 03:35:33 -0500

    0626_1

--- controllers/routeController.js
+++ controllers/routeController.js
@@ -18,11 +18,11 @@ myApp.component('navMenu',
 {template:
   '<nav class="d-flex justify-content-center">'+
     '<ul class="nav nav-pills">'+
-      '<li >'+'<a class="btn btn-outline-primary" href="#">'+'Home'+'</a>'+'</li>'+
-      '<li >'+'<a class="btn btn-outline-primary" href="#/mytech">'+'What I know'+'</a>'+'</li>'+
-      '<li >'+'<a class="btn btn-outline-primary" href="#/contact">'+'Contact me'+'</a>'+'</li>'+
-      '<li >'+'<a class="btn btn-outline-primary" href="#/blog" >'+'Blog'+'</a>'+'</li>'+
-      '<li >'+'<a class="btn btn-outline-primary" href="#/utility" >'+'Utilities'+'</a>'+'</li>'+
+      '<li >'+'<a class="btn btn-light" href="#">'+'Home'+'</a>'+'</li>'+
+      '<li >'+'<a class="btn btn-light" href="#/mytech">'+'What I know'+'</a>'+'</li>'+
+      '<li >'+'<a class="btn btn-light" href="#/contact">'+'Contact me'+'</a>'+'</li>'+
+      '<li >'+'<a class="btn btn-light" href="#/blog" >'+'Blog'+'</a>'+'</li>'+
+      '<li >'+'<a class="btn btn-light" href="#/utility" >'+'Utilities'+'</a>'+'</li>'+
     '</ul>'+
   '</nav>'
 });
