e4b4d40ec1792e9d5cadb5a4ef06c34f1cc1ba95
Abdul Raheem <actekraheem@gmail.com>
2018-06-27 13:56:31 -0500

    0627_1

--- controllers/navtemplatectrl.js
+++ controllers/navtemplatectrl.js
@@ -1,12 +1,19 @@
 myApp.component('navMenu',
 {template:
-  '<nav class="d-flex justify-content-center">'+
-    '<ul class="nav nav-pills">'+
-      '<li >'+'<a class="btn btn-light border" href="#">'+'Home'+'</a>'+'</li>'+
-      '<li >'+'<a class="btn btn-light border" href="#/exp" >'+'Experience'+'</a>'+'</li>'+
-      '<li >'+'<a class="btn btn-light border" href="#/mytech">'+'Technology'+'</a>'+'</li>'+
-      '<li >'+'<a class="btn btn-light border" href="#/contact">'+'Contact me'+'</a>'+'</li>'+
-      '<li >'+'<a class="btn btn-light border" href="#/utility" >'+'Utilities'+'</a>'+'</li>'+
-    '</ul>'+
-  '</nav>'
+  '<nav class="navbar navbar-dark bg-dark navbar-expand-sm">'+
+  '<div class="container">'+
+    '<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">'+
+        '<span class="navbar-toggler-icon"></span>'+
+    '</button>'+
+          '<div class="collapse navbar-collapse" id="navbarToggleExternalContent">'+
+          '<div class="navbar-nav">'+
+            '<a class="nav-item nav-link" href="#">'+'Home'+'</a>'+
+            '<a class="nav-item nav-link" href="#/exp">'+'Experience'+'</a>'+
+            '<a class="nav-item nav-link" href="#/mytech">'+'Technology'+'</a>'+
+            '<a class="nav-item nav-link" href="#/contact">'+'Contact me'+'</a>'+
+            '<a class="nav-item nav-link" href="#/utility">'+'Utilities'+'</a>'+
+          '</div>'+
+        '</div>'+
+      '</div>'+
+    '</nav>'
 });
